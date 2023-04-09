31c1b77afb6bd6510ce57fd92b8c9f6392debb0d
sergiu <abc@def.com>
2023-04-09 15:07:14 +0300

    Initial Commit

---
+++ Jumbo/Jumbo.h
@@ -0,0 +1,18 @@
+//
+//  Jumbo.h
+//  Jumbo
+//
+//  Created by Sergiu Todirascu on 01.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jumbo.
+FOUNDATION_EXPORT double JumboVersionNumber;
+
+//! Project version string for Jumbo.
+FOUNDATION_EXPORT const unsigned char JumboVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jumbo/PublicHeader.h>
+
+
